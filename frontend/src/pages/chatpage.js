6bda822472452f3a10b0db9396ea1cc90ed79f48
Max Kim <maxkim@berkeley.edu>
2022-01-06 19:19:14 -0800

    Folder Structure, Initialized frontend structure using Create-React-App, and Home, Chat Page Creation

---
+++ frontend/src/pages/chatpage.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+const Chatpage = () => {
+
+    return (
+    <div> Chat
+    </div>
+    );
+};
+
+export default Chatpage
---
+++ frontend/src/pages/homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
+
+const Homepage = () => {
+    return (
+    <div>Home</div>
+    )
+}
+
+export default Homepage
